7cbb80322fce379a56c63ed78b6041fa3516cb87
Joel <Joel@DESKTOP-E4SG2VV>
2024-07-14 00:37:50 +1000

    Moved to src directory

--- compile.bat
+++ compile.bat
@@ -2,6 +2,6 @@ cls
 
 if not exist ".\build\" mkdir .\build\
 
-cl /D DEBUG=1 /ZI /EHsc /W3 /Febuild/Win32DX11.exe PlatformWin32DX11.cpp
+cl /D DEBUG=1 /ZI /EHsc /W3 /Febuild/Win32DX11.exe src\PlatformWin32DX11.cpp
 
-cl /O2 /Febuild/Win32DX11Release.exe PlatformWin32DX11.cpp
+cl /O2 /Febuild/Win32DX11Release.exe src\PlatformWin32DX11.cpp
